dcf61d24147a2fd898250fe8eb17c2cf155a1c8e
Oxymoron <daniel.m.bihlmaier@gmail.com>
2021-09-24 15:51:30 +0200

    default test socket.io project

--- server/src/build/index.js
+++ server/src/build/index.js
@@ -6,10 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("dotenv");
+const process_1 = require("process");
+const socket_io_1 = require("socket.io");
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
 const server = (0, http_1.createServer)(app);
-server.listen(process.env['SERVER_PORT'] || 3000, () => {
-    console.log("listening on port: ", process.env['SERVER_PORT'] || 3000);
+const io = new socket_io_1.Server(server);
+app.use(express_1.default.static("public"));
+app.all("/online", (_req, res) => {
+    res.json({ status: "yes.", STATUS: "YES." });
+});
+io.on('connection', (socket) => {
+    console.log("cOnnected: " + socket.id);
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+});
+server.listen(process_1.env['SERVER_PORT'] || 3000, () => {
+    console.log("listening on *:" + process_1.env['SERVER_PORT'] || 3000);
 });
 //# sourceMappingURL=index.js.map
--- server/src/ts/index.ts
+++ server/src/ts/index.ts
@@ -1,11 +1,28 @@
 import { createServer } from "http";
 import express from 'express';
 import { config } from 'dotenv';
+import { env } from 'process';
+import { Server } from 'socket.io';
+
 config();
 
 const app = express()
 const server = createServer(app);
+const io = new Server(server)
 
-server.listen(process.env['SERVER_PORT'] || 3000, () => {
-  console.log("listening on port: ", process.env['SERVER_PORT'] || 3000)
+app.use(express.static("public"))
+
+app.all("/online", (_req, res) => {
+  res.json({ status: "yes.", STATUS: "YES." })
+})
+
+io.on('connection', (socket) => {
+
+  console.log("cOnnected: " + socket.id)
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+server.listen(env['SERVER_PORT'] || 3000, () => {
+  console.log("listening on *:" + env['SERVER_PORT'] || 3000)
 })
